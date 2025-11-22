2eb5ff7f69ba768497186fae5d02edf82251364c
Andre Alves <andrecambridge@gmail.com>
2025-11-21 21:52:28 -0300

    feat: change cert domain

--- infra/modules/acm/03-variables.tf
+++ infra/modules/acm/03-variables.tf
@@ -2,3 +2,8 @@ variable "domain_name" {
   description = "The domain name for which to create the SSL certificate."
   type        = string
 }
+
+variable "project" {
+  description = "The name of the project, used for resource naming."
+  type        = string
+}
